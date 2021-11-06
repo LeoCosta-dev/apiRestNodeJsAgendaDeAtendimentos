d17254b1dd21fc3bf01411478e3ab53cb56e429f
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-11-06 01:50:06 -0300

    alterações na conexão

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,14 +1,13 @@
+require('dotenv').config()
 //importação do mysql
-require('dotenv').config
-
 const mysql = require('mysql2');
 //configuração da conexão do mysql
 const conexao = mysql.createConnection({
-    host: HOSTNAME_BD,
+    host: process.env.HOSTNAME_BD,
     port: 3306,
-    user: LOGIN_BD,
-    password: SENHA_BD,
-    database: BANCO_DE_DADOS
+    user: process.env.LOGIN_BD,
+    password: process.env.SENHA_BD,
+    database: process.env.BANCO_DE_DADOS
 });
 
 // exportar conexão.
